d65e619bb9686c453cb4f21dcf7f169e80dfd51b
Angel Szr <123338511+AngelIsaiSzr@users.noreply.github.com>
2023-06-10 21:47:56 -0600

    Update Generador de Contraseñas.py

--- Python/Generador de Contraseñas.py
+++ Python/Generador de Contraseñas.py
@@ -11,25 +11,17 @@ class Password:
         self.master = master
         master.title("Generador de Contraseñas")
 
-        # Crea una etiqueta para mostrar el mensaje "Generando Contraseña"
-        self.label_generando = tk.Label(master, text="Generando Contraseña...", font=('Helvetica', 16, 'bold'))
-        self.label_generando.pack()
+        # Crea una etiqueta para mostrar el mensaje "Longitud de la Contraseña"
+        self.label_longitud = tk.Label(master, text="Longitud de la Contraseña:", font=('Helvetica', 15, 'bold'))
+        self.label_longitud.pack()
+
+        # Crea un cuadro de entrada de texto para la longitud de la contraseña
+        self.entry_longitud = tk.Entry(master, font=('Helvetica', 16))
+        self.entry_longitud.pack()
 
         # Crea una etiqueta para mostrar el mensaje "Contraseña"
-        self.label_contrasena = tk.Label(master, text="Contraseña:", font=('Helvetica', 15, 'bold'))
+        self.label_contrasena = tk.Label(master, text="Ingresa la longitud de la contraseña y genera una nueva", font=('Helvetica', 15, 'bold'))
         self.label_contrasena.pack()
-
-        # Crea una cadena de caracteres que se utilizará para generar la contraseña
-        lower = string.ascii_lowercase  # Letras minúsculas
-        upper = string.ascii_lowercase  # Letras mayúsculas
-        num = string.digits  # Números
-        symbols = string.punctuation  # Símbolos
-        chars = lower + upper + num + symbols  # Combinación de los caracteres
-        self.temp = random.sample(chars, 10)  # Genera una lista aleatoria de 10 caracteres
-
-        # Crea una etiqueta para mostrar la contraseña
-        self.label = tk.Label(master, text="".join(self.temp), font=('Helvetica', 16))
-        self.label.pack()
 
         # Crea un contenedor para la contraseña y los botones
         self.container = tk.Frame(master)
@@ -46,26 +38,37 @@ class Password:
         self.button_copiar.pack(side='left', padx=5, pady=5)
 
     def click(self):
-        # Actualiza la etiqueta de "Generando Contraseña"
-        self.label_generando.config(text="Generando Contraseña...")
+        # Verifica si se ingresó una longitud de contraseña válida
+        try:
+            longitud = int(self.entry_longitud.get())
+        except ValueError:
+            self.label_contrasena.config(text="Ingresa una longitud de contraseña válida")
+            return
 
-        # Genera una nueva lista aleatoria de caracteres para la contraseña
+        # Verifica si la longitud de contraseña ingresada es mayor que cero
+        if longitud <= 0:
+            self.label_contrasena.config(text="La longitud de la contraseña debe ser mayor que cero")
+            return
+
+        # Actualiza la etiqueta de "Contraseña"
+        self.label_contrasena.config(text="Contraseña:")
+
+        # Genera una lista aleatoria de caracteres para la contraseña
         lower = string.ascii_lowercase
-        upper = string.ascii_lowercase
+        upper = string.ascii_uppercase
         num = string.digits
         symbols = string.punctuation
         chars = lower + upper + num + symbols
-        self.temp = random.sample(chars, 10)
+        self.temp = random.sample(chars, longitud)
 
         # Actualiza la etiqueta de la contraseña con la nueva contraseña generada
-        self.label.config(text="".join(self.temp))
+        self.label_contrasena.config(text="Contraseña: " + "".join(self.temp))
 
     def copiar(self):
         # Copia la contraseña al portapapeles
         pyperclip.copy("".join(self.temp))
         # Imprime un mensaje indicando que la contraseña ha sido copiada al portapapeles
         print("Contraseña copiada al portapapeles.")
-
 
 # Crea la ventana principal y la instancia de la clase Password
 root = tk.Tk()
